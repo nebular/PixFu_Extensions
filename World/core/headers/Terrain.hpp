d0bd475e93387ddf65b2225a8f1c6b9a00c21e70
rodomac <rodopos@nebular.tv>
2020-03-02 19:32:37 +0100

    more refactors

--- World/core/headers/Terrain.hpp
+++ World/core/headers/Terrain.hpp
@@ -17,25 +17,23 @@
 
 namespace Pix {
 
-
-//////////////////////////////////////////////////////////////////////////////////////////
-
-
 class Terrain : public LayerVao {
 
 		static std::string TAG;
 
-		Canvas2D *pDirtCanvas = nullptr;
-		Texture2D *pDirtTexture = nullptr;
-		Texture2D *pTexture = nullptr;
-		Drawable *pHeightMap = nullptr;
-		ObjLoader *pLoader = nullptr;
+		Texture2D *pTexture = nullptr;		// Terrain texture
+		Texture2D *pDirtTexture = nullptr;	// 3D canvas texture
+		Canvas2D *pDirtCanvas = nullptr;	// 3D canvas over the texture
+		Drawable *pHeightMap = nullptr;		// Height Map
+		ObjLoader *pLoader = nullptr;		// 3D model loader
 
 		/** Terrain Size */
 		glm::vec2 mSize;
 
+		/** Whether terrain has been inited */
 		bool bInited = false;
 
+		/** Inits the terrain */
 		void init(TerrainShader *shader);
 
 	public:
